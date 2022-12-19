c9f33845c7b3d25bf5165b34826ffe858fc4ca2b
Aurélien GARNIER <au2001sfg@gmail.com>
2022-12-19 14:49:56 +0100

    Remove update token button

--- src/components/TokenInfoForm.tsx
+++ src/components/TokenInfoForm.tsx
@@ -7,7 +7,7 @@ export interface TokenData {
 	tokenId: string;
 }
 
-export function TokenInfoForm(props: { onChangeToken?: (tokenData: TokenData) => void }) {
+export function TokenInfoForm(props: { onChangeToken?: (tokenData: TokenData | null) => void }) {
 	// const [tokenType, setTokenType] = React.useState<ItemType | undefined>(undefined);
 	// const [tokenAddress, setTokenAddress] = React.useState("");
 	// const [tokenId, setTokenId] = React.useState("");
@@ -15,24 +15,24 @@ export function TokenInfoForm(props: { onChangeToken?: (tokenData: TokenData) =>
 	const [tokenAddress, setTokenAddress] = React.useState("0xf4910c763ed4e47a585e2d34baa9a4b611ae448c");
 	const [tokenId, setTokenId] = React.useState("52825296347877427141239656042084453522409464454284240208904887735097764085770");
 
-	function handleSubmit() {
+	React.useEffect(() => {
+		if (props.onChangeToken === undefined) return;
+
 		if (tokenType !== ItemType.ERC721 && tokenType !== ItemType.ERC1155)
-			return alert("Invalid token type.");
+			return props.onChangeToken(null); // Invalid token type.
 
 		if (!tokenAddress.startsWith("0x") || tokenAddress.length !== 42)
-			return alert("Invalid token address.");
+			return props.onChangeToken(null); // Invalid token address.
 
 		if (!tokenId.match(/^\d+$/))
-			return alert("Invalid token ID.");
+			return props.onChangeToken(null); // Invalid token ID.
 
-		if (props.onChangeToken) {
-			props.onChangeToken({
-				tokenType,
-				tokenAddress,
-				tokenId,
-			});
-		}
-	}
+		props.onChangeToken({
+			tokenType,
+			tokenAddress,
+			tokenId,
+		});
+	}, [props, tokenType, tokenAddress, tokenId]);
 
 	return (
 		<div id="token-info" className="section">
@@ -63,9 +63,6 @@ export function TokenInfoForm(props: { onChangeToken?: (tokenData: TokenData) =>
 				placeholder="Token ID"
 				value={tokenId}
 			/>
-			<br/>
-
-			<button onClick={handleSubmit}>Update Token Info</button>
 		</div>
 	);
 }
